45be493e77773d738c942a539eebecf041a0b2ac
lubomyr <lubomyr31@gmail.com>
2016-04-19 19:19:58 +0300

    updated sdl sound

--- src/sd-sdl/sound_sdl_new.cpp
+++ src/sd-sdl/sound_sdl_new.cpp
@@ -43,7 +43,6 @@ uae_u16 *finish_sndbuff = sndbuffer[0] + SNDBUFFER_LEN*2;
 
 #ifdef NO_SOUND
 
-
 void finish_sound_buffer (void) {  }
 
 int setup_sound (void) { sound_available = 0; return 0; }
@@ -62,14 +61,6 @@ void update_sound (int) { }
 
 void reset_sound (void) { }
 
-void uae4all_init_sound(void) { }
-
-void uae4all_play_click(void) { }
-
-void uae4all_pause_music(void) { }
-
-void uae4all_resume_music(void) { }
-
 void restart_sound_buffer(void) { }
 
 #else 
@@ -81,7 +72,7 @@ static int lastfreq;
 void update_sound (int freq, int lof)
 {
   float lines = maxvpos_nom;
-	float hpos = maxhpos;
+  float hpos = maxhpos;
   float evtime;
 
   if (freq < 0)
@@ -99,34 +90,32 @@ void update_sound (int freq, int lof)
 
 static int s_oldrate = 0, s_oldbits = 0, s_oldstereo = 0;
 static int sound_thread_active = 0, sound_thread_exit = 0;
-static sem_t sound_sem, callback_sem;
+static sem_t sound_sem, sound_out_sem;
+static int output_cnt = 0;
 
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
-static int cnt = 0;
 
 static void sound_thread_mixer(void *ud, Uint8 *stream, int len)
 {
-	if (sound_thread_exit) return;
-	int sem_val;
+	int cnt = 0, sem_val = 0;
 	sound_thread_active = 1;
-	/*
-	// Sound is choppy, arrgh
+	
 	sem_getvalue(&sound_sem, &sem_val);
+	
 	while (sem_val > 1)
 	{
-		//printf("skip %i (%i)\n", cnt, sem_val);
 		sem_wait(&sound_sem);
-		cnt++;
 		sem_getvalue(&sound_sem, &sem_val);
 	}
-	*/
 
-#ifdef SOUND_USE_SEMAPHORES
 	sem_wait(&sound_sem);
-	sem_post(&callback_sem);
-#endif
-	cnt++;
-	//__android_log_print(ANDROID_LOG_INFO, "UAE4ALL2","Sound callback cnt %d buf %d\n", cnt, cnt%SOUND_BUFFERS_COUNT);
+	
+	if (sound_thread_exit) 
+	  return;
+
+        cnt = output_cnt;
+        sem_post(&sound_out_sem);
+    
 	if(currprefs.sound_stereo)
 		memcpy(stream, sndbuffer[cnt%SOUND_BUFFERS_COUNT], MIN(SNDBUFFER_LEN*2, len));
 	else
@@ -145,7 +134,7 @@ static int pandora_start_sound(int rate, int bits, int stereo)
 		// init sem, start sound thread
 		printf("starting sound thread..\n");
 		ret = sem_init(&sound_sem, 0, 0);
-		sem_init(&callback_sem, 0, 0);
+		sem_init(&sound_out_sem, 0, 0);
 		if (ret != 0) printf("sem_init() failed: %i, errno=%i\n", ret, errno);
 	}
 
@@ -202,37 +191,17 @@ void pandora_stop_sound(void)
 static int wrcnt = 0;
 void finish_sound_buffer (void)
 {
+  output_cnt = wrcnt;
 
-#ifdef DEBUG_SOUND
-	dbg("sound.c : finish_sound_buffer");
-#endif
-
-	//printf("finish %i\n", wrcnt);
-#if 0
-	if(currprefs.sound_stereo)
-	write(sounddev, sndbuffer[0], SNDBUFFER_LEN*2);
-	else
-	write(sounddev, sndbuffer[0], SNDBUFFER_LEN);  
-	sndbufpt = render_sndbuff = sndbuffer[0];
-#else
-
-#ifdef SOUND_USE_SEMAPHORES
 	sem_post(&sound_sem);
-	sem_wait(&callback_sem);
-#endif
+	sem_wait(&sound_out_sem);
+  
 	wrcnt++;
-	sndbufpt = render_sndbuff = sndbuffer[wrcnt%SOUND_BUFFERS_COUNT];
-	//__android_log_print(ANDROID_LOG_INFO, "UAE4ALL2","Sound buffer write cnt %d buf %d\n", wrcnt, wrcnt%SOUND_BUFFERS_COUNT);
-#endif
-
+	sndbufpt = render_sndbuff = sndbuffer[wrcnt&3];
 	if(currprefs.sound_stereo)
 	  finish_sndbuff = sndbufpt + SNDBUFFER_LEN;
 	else
-	  finish_sndbuff = sndbufpt + SNDBUFFER_LEN/2;	
-
-#ifdef DEBUG_SOUND
-	dbg(" sound.c : ! finish_sound_buffer");
-#endif
+	  finish_sndbuff = sndbufpt + SNDBUFFER_LEN/2;	  
 }
 
 
@@ -248,143 +217,56 @@ void restart_sound_buffer(void)
 /* Try to determine whether sound is available.  This is only for GUI purposes.  */
 int setup_sound (void)
 {
-#ifdef DEBUG_SOUND
-    dbg("sound.c : setup_sound");
-#endif
+  if (pandora_start_sound(currprefs.sound_freq, 16, currprefs.sound_stereo) != 0)
+    return 0;
 
-     // Android does not like opening sound device several times
-     if (pandora_start_sound(currprefs.sound_freq, 16, currprefs.sound_stereo) != 0)
-        return 0;
-
-     sound_available = 1;
-
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! setup_sound");
-#endif
-    return 1;
+  sound_available = 1;
+  return 1;
 }
 
 static int open_sound (void)
 {
-#ifdef DEBUG_SOUND
-    dbg("sound.c : open_sound");
-#endif
-
-	// Android does not like opening sound device several times
-    if (pandora_start_sound(currprefs.sound_freq, 16, currprefs.sound_stereo) != 0)
+  if (pandora_start_sound(currprefs.sound_freq, 16, currprefs.sound_stereo) != 0)
 	    return 0;
 
-//    init_sound_table16 ();
+  have_sound = 1;
+  sound_available = 1;
 
-    have_sound = 1;
-    sound_available = 1;
-
-    if(currprefs.sound_stereo)
-      sample_handler = sample16s_handler;
-    else
-      sample_handler = sample16_handler;
+  if(currprefs.sound_stereo)
+    sample_handler = sample16s_handler;
+  else
+    sample_handler = sample16_handler;
  
-
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! open_sound");
-#endif
-    return 1;
+  return 1;
 }
 
 void close_sound (void)
 {
-#ifdef DEBUG_SOUND
-    dbg("sound.c : close_sound");
-#endif
-    if (!have_sound)
-	return;
+  if (!have_sound)
+	  return;
 
-    // testing shows that reopenning sound device is not a good idea (causes random sound driver crashes)
-    // we will close it on real exit instead
-#ifdef RASPBERRY
-    //pandora_stop_sound();
-#endif
-    have_sound = 0;
-
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! close_sound");
-#endif
+  // testing shows that reopenning sound device is not a good idea on pandora (causes random sound driver crashes)
+  // we will close it on real exit instead
+  //pandora_stop_sound();
+  have_sound = 0;
 }
 
 int init_sound (void)
 {
-#ifdef DEBUG_SOUND
-    dbg("sound.c : init_sound");
-#endif
-
     have_sound=open_sound();
-
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! init_sound");
-#endif
     return have_sound;
 }
 
 void pause_sound (void)
 {
-#ifdef DEBUG_SOUND
-    dbg("sound.c : pause_sound");
-#endif
-
 	SDL_PauseAudio (1);
     /* nothing to do */
-
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! pause_sound");
-#endif
 }
 
 void resume_sound (void)
 {
-#ifdef DEBUG_SOUND
-    dbg("sound.c : resume_sound");
-#endif
-
 	SDL_PauseAudio (0);
     /* nothing to do */
-
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! resume_sound");
-#endif
-}
-
-void uae4all_init_sound(void)
-{
-#ifdef DEBUG_SOUND
-    dbg("sound.c : uae4all_init_sound");
-#endif
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! uae4all_init_sound");
-#endif
-}
-
-void uae4all_pause_music(void)
-{
-#ifdef DEBUG_SOUND
-    dbg("sound.c : pause_music");
-#endif
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! pause_music");
-#endif
-}
-
-void uae4all_resume_music(void)
-{
-#ifdef DEBUG_SOUND
-    dbg("sound.c : resume_music");
-#endif
-#ifdef DEBUG_SOUND
-    dbg(" sound.c : ! resume_music");
-#endif
-}
-
-void uae4all_play_click(void)
-{
 }
 
 void reset_sound (void)
@@ -394,7 +276,6 @@ void reset_sound (void)
 
   memset(sndbuffer, 0, 2 * 4 * (SNDBUFFER_LEN+32)*DEFAULT_SOUND_CHANNELS);
 }
-
 
 void sound_volume (int dir)
 {
