cd0cced06a11120c9b96f248e6f2e25c8721eccc
lubomyr <lubomyr31@gmail.com>
2017-05-15 23:57:48 +0300

    Hide system mouse cursor for Android 7.x and higher

--- AndroidBuild.sh
+++ AndroidBuild.sh
@@ -4,7 +4,7 @@
 LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
-JOBS=1
+JOBS=4
 
 ln -sf libsdl_ttf.so $LOCAL_PATH/../../../obj/local/$1/libSDL_ttf.so
 ln -sf libguichan.so $LOCAL_PATH/../../../obj/local/$1/libguichan_sdl.so
